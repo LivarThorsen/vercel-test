f81af9bf791c8c8da7e6202e2bcf8f48490ddbcc
Livar.Thorsen <livar.thorsen@gmail.com>
2025-03-19 23:11:31 +0100

    asdf

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const connection = {
   host: process.env.HOST,
   port: process.env.DB_PORT,
   dialect: process.env.DIALECT,
-  // dialectmodel: process.env.DIALECTMODEL,
+  dialectmodel: process.env.DIALECTMODEL,
   dialectmodel: require('mysql2'),
 };
 const sequelize = new Sequelize(connection);
@@ -18,11 +18,11 @@ const db = {}
 db.sequelize = sequelize
 fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) &&   
+    return (file.indexOf('.') !== 0) && (file !== basename) &&
       (file.slice(-3) === '.js');
-    })
-  .forEach(file => {    
-    const model = require(path.join(__dirname, file))(sequelize,   
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize,
       Sequelize);
     db[model.name] = model;
   });
